229d8bfb1b04c8a257e7afee2dc2b4e91087b5f6
Jethro Tuburan <tuburanjethro@gmail.com>
2019-04-17 11:57:57 +1200

    Added level 1 security. (Database of users)

--- app.js
+++ app.js
@@ -1,1 +1,71 @@
 //jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.route("/login")
+    .get(function (req, res) {
+        res.render("login");
+    })
+    .post(function(req, res){
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne(
+            {email: username, password: password},
+            function(err, foundUser){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    res.render("secrets");  
+                }
+            }
+        )
+    })
+
+app.route("/register")
+    .get(function (req, res) {
+        res.render("register");
+    })
+    .post(function (req, res) {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save(function(err){
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render("secrets");
+            }
+        });
+    });
+
+app.listen(3000, function () {
+    console.log("Server has started on port 3000");
+})
